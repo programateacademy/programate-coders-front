2ad5019095d695d33b9cfbedf1c7a4892604c814
Giovannyhs <giovannyhs@outlook.com>
2023-08-18 02:09:02 -0500

    carrusel

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
+import React from "react";
+import Carrusel from "./Components/Carrusel";
 
+function App() {
+  return (
+    <div className="App">
+      <Carrusel />
+    </div>
+  );
+}
+
+export default App;
--- src/Components/Carrusel.jsx
+++ src/Components/Carrusel.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import "../Styles/Carrusel.css";
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+
+// import required modules
+import { EffectCoverflow, Pagination } from "swiper/modules";
 
 function Carrusel() {
   return (
-    <div>Carrusel</div>
-  )
+    <>
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="./imagenes/html.svg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="./imagenes/css3.svg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="./imagenes/js.svg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="./imagenes/python.svg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="./imagenes/github.svg" alt="" />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
 }
 
-export default Carrusel
+export default Carrusel;
