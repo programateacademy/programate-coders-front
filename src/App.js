fdfdd1bfbafb0a4c2f8f869238dce90ab7979270
jorget0rr3s <torresor.jt@gmail.com>
2023-08-18 18:16:37 -0500

    fix change app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,14 @@
+import React from "react";
+import Carrusel from "./Components/Carrusel";
+import Header from "./Components/Header"
 
+function App() {
+    return (
+        <div className="App">
+            <Header />
+            <Carrusel />
+        </div>
+    );
+}
 
-export default App
+export default App;
