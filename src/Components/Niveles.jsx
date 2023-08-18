a2df6a0e9e5d5e69d3d569b8a96038aafa620646
fbeltrann <pipebeltran1019@gmail.com>
2023-08-17 21:14:17 -0500

    niveles.jsx

---
+++ src/Components/Niveles.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import './Niveles.css';
+
+function Niveles() {
+  const [selectedLevel, setSelectedLevel] = useState(null);
+
+  const handleLevelClick = (level) => {
+    setSelectedLevel(level);
+  };
+
+  return (
+    <div className="Niveles">
+      <div className="barra">
+        <div
+          className={`nivel ${selectedLevel === 1 ? 'selected' : ''}`}
+          onClick={() => handleLevelClick(1)}
+        >
+          Nivel 1
+        </div>
+        <div
+          className={`nivel ${selectedLevel === 2 ? 'selected' : ''}`}
+          onClick={() => handleLevelClick(2)}
+        >
+          Nivel 2
+        </div>
+        <div
+          className={`nivel ${selectedLevel === 3 ? 'selected' : ''}`}
+          onClick={() => handleLevelClick(3)}
+        >
+          Nivel 3
+        </div>
+      </div>
+      {selectedLevel !== null && (
+        <div className="contenido">
+          Contenido del Nivel {selectedLevel} desplegado.
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Niveles;
