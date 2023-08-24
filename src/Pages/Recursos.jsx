df65937141e7136d625a25dfdea5593f50eb43bb
jorget0rr3s <torresor.jt@gmail.com>
2023-08-23 21:24:00 -0500

    fix: changes page recurson, include videosection

--- src/Pages/Recursos.jsx
+++ src/Pages/Recursos.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import Header from "../Components/Header"
 import CardsAcademy from "../Components/CardsAcademy"
 import Niveles from "../Components/Niveles"
-
+import VideoSection from "../Components/VideoSection"
 function Recursos () {
     return (
         <>
         <Header/>
         <CardsAcademy/>
+        <VideoSection/>
         <Niveles/>
         </>
     )
