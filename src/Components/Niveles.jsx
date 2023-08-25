e0575bc02e144bed6fdb4ccaffa1c9472015cc68
jorget0rr3s <torresor.jt@gmail.com>
2023-08-25 11:28:53 -0500

    fix: commit niveles component

--- src/Components/Niveles.jsx
+++ src/Components/Niveles.jsx
@@ -1,50 +1,50 @@
-import React, { useState } from 'react';
-import { Document, Page, pdfjs } from 'react-pdf';
-import '../Styles/Niveles.css';
+// import React, { useState } from 'react';
+// import { Document, Page, pdfjs } from 'react-pdf';
+// import '../Styles/Niveles.css';
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-function Niveles() {
-  const [selectedLevel, setSelectedLevel] = useState(null);
-  const [pdfFile, setPdfFile] = useState(null);
+// function Niveles() {
+//   const [selectedLevel, setSelectedLevel] = useState(null);
+//   const [pdfFile, setPdfFile] = useState(null);
 
-  const handleLevelClick = (level) => {
-    setSelectedLevel(level);
-    setPdfFile(`/pdfs/Nivel${level}.pdf`);
-  };
+//   const handleLevelClick = (level) => {
+//     setSelectedLevel(level);
+//     setPdfFile(`/pdfs/Nivel${level}.pdf`);
+//   };
 
-  return (
-    <div className="Niveles">
-      <div className="barra">
-        <div
-          className={`nivel ${selectedLevel === 1 ? 'selected' : ''}`}
-          onClick={() => handleLevelClick(1)}
-        >
-          Nivel 1
-        </div>
-        <div
-          className={`nivel ${selectedLevel === 2 ? 'selected' : ''}`}
-          onClick={() => handleLevelClick(2)}
-        >
-          Nivel 2
-        </div>
-        <div
-          className={`nivel ${selectedLevel === 3 ? 'selected' : ''}`}
-          onClick={() => handleLevelClick(3)}
-        >
-          Nivel 3
-        </div>
-      </div>
-      {selectedLevel !== null && (
-        <div className="contenido">
-          Contenido del Nivel {selectedLevel} desplegado.
-          <Document file={pdfFile}>
-            <Page pageNumber={1} />
-          </Document>
-        </div>
-      )}
-    </div>
-  );
-}
+//   return (
+//     <div className="Niveles">
+//       <div className="barra">
+//         <div
+//           className={`nivel ${selectedLevel === 1 ? 'selected' : ''}`}
+//           onClick={() => handleLevelClick(1)}
+//         >
+//           Nivel 1
+//         </div>
+//         <div
+//           className={`nivel ${selectedLevel === 2 ? 'selected' : ''}`}
+//           onClick={() => handleLevelClick(2)}
+//         >
+//           Nivel 2
+//         </div>
+//         <div
+//           className={`nivel ${selectedLevel === 3 ? 'selected' : ''}`}
+//           onClick={() => handleLevelClick(3)}
+//         >
+//           Nivel 3
+//         </div>
+//       </div>
+//       {selectedLevel !== null && (
+//         <div className="contenido">
+//           Contenido del Nivel {selectedLevel} desplegado.
+//           <Document file={pdfFile}>
+//             <Page pageNumber={1} />
+//           </Document>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
 
-export default Niveles;
+// export default Niveles;
