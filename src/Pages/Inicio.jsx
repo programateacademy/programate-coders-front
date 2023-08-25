d1817853d187f55cea6a1f29d720093d39fb5835
juancmb27 <juanmontealegreb.27@gmail.com>
2023-08-24 20:49:31 -0500

    Footer

--- src/Pages/Inicio.jsx
+++ src/Pages/Inicio.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from "../Components/Header"
 import '../Styles/Inicio.css'
+import Footer from "../Components/Footer"
 
 function Inicio() {
     return (
         <div className='Container-programate'>
             <Header />
+            
             <h1>Bienvenido a Programate</h1>
             <div className='Container-description'>
                 <p>Prográmate es un bootcamp de desarrollo web. <br />
@@ -16,6 +18,7 @@ function Inicio() {
             <div className='Container-video'>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/8oY2riIbxg4?si=1bzUky5r9v5r8s3p" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
+            <Footer/>
         </div>
     )
 }
