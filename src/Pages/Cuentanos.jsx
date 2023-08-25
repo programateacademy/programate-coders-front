789405f0fe6e64a383cc0142a081b372388c7b4c
jorget0rr3s <torresor.jt@gmail.com>
2023-08-25 13:41:27 -0500

    fix: change formulario file

--- src/Pages/Cuentanos.jsx
+++ src/Pages/Cuentanos.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import Header from "../Components/Header"
-import CardsSchool from "../Components/CardsSchool"
-import Niveles from "../Components/Niveles"
+import Formulario from '../Components/Formulario'
 
 function Cuentanos () {
     return (
         <>
         <Header/>
-        <CardsSchool/>
-        <Niveles/>
+        <Formulario/>
         </>
     )
 }
