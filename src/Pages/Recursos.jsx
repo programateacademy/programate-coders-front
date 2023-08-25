41248931f96739683e857d693246c19b9743ad89
Giovannyhs <giovannyhs@outlook.com>
2023-08-25 16:56:39 -0500

    cambios de responsive

--- src/Pages/Recursos.jsx
+++ src/Pages/Recursos.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import Header from "../Components/Header"
-import CardsAcademy from "../Components/CardsAcademy"
-import Niveles from "../Components/Niveles"
-import VideoSection from "../Components/VideoSection"
-function Recursos () {
-    return (
-        <>
-        <Header/>
-        <CardsAcademy/>
-        <VideoSection/>
-        <Niveles/>
-        </>
-    )
+import React from "react";
+import Header from "../Components/Header";
+import CardsAcademy from "../Components/CardsAcademy";
+import Niveles from "../Components/Niveles";
+import VideoSection from "../Components/VideoSection";
+function Recursos() {
+  return (
+    <>
+      <Header />
+      <CardsAcademy />
+      <VideoSection />
+      <Niveles />
+    </>
+  );
 }
 
-export default Recursos
+export default Recursos;
