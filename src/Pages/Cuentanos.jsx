677934f98a0a568c5c9bb0a0109dfc44feaf631a
juancmb27 <juanmontealegreb.27@gmail.com>
2023-08-25 13:54:32 -0500

    2

--- src/Pages/Cuentanos.jsx
+++ src/Pages/Cuentanos.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import Header from "../Components/Header"
-import CardsSchool from "../Components/CardsSchool"
-import Niveles from "../Components/Niveles"
+import Formulario from "../Components/Formulario"
 import Footer from '../Components/Footer'
 
 function Cuentanos () {
     return (
         <>
         <Header/>
-        <CardsSchool/>
-        <Niveles/>
+        <Formulario/>
         <Footer/>
         </>
     )
