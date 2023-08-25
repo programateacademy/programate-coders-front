415ade1add2b7bd00842c36bd3b8da96d4583feb
jorget0rr3s <torresor.jt@gmail.com>
2023-08-25 18:51:56 -0500

    feat: add files.js

---
+++ src/DataBases/files.js
@@ -0,0 +1,150 @@
+const InfoData = [
+    {
+        program: "Programate Academy",
+        cards: [
+            {
+                name: "JavaScript",
+                levels: [
+                    {
+                        level: 1,
+                        infoType: "Workbook",
+                        resources: [
+                            {
+                                title: "Workbook Javascript",
+                                type: "PDF",
+                                link: "https://drive.google.com/file/d/1hemzW5EWBbPmtX9_cSWXvF0mgRKbAjYj/view?usp=sharing",
+                            },
+                            {
+                                title: "Workbook nivelacion JS",
+                                type: "PDF",
+                                link: "https://drive.google.com/file/d/1yly_Q1SQYTHGaEm0bZ5rXQyS0zYgRC-V/view?usp=sharing",
+                            },
+                            {
+                                title: "Wokbook JavaScript",
+                                type: "PDF",
+                                link: "https://drive.google.com/drive/folders/1U_47SMq8j8dru6w0CgomVTGg_XbGoNms",
+                            },
+                        ],
+                    },
+                    {
+                        level: 1,
+                        infoType: "proyectos",
+                        resources: [],
+                    },
+                    {
+                        level: 1,
+                        infoType: "recursos",
+                        resources: [
+                            {
+                                title: "Programación orientada a objetos",
+                                type: "PDF",
+                                link: "https://drive.google.com/file/d/14eE35J1b9kcY_dTgCuLoTtQ27Nro1XxT/view?usp=sharing",
+                            },
+                            {
+                                title: "Ejercicios prácticos",
+                                type: "URL",
+                                link: "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/",
+                            },
+                        ],
+                    },
+                    // Más niveles...
+                ],
+            },
+            {
+                name: "CSS",
+                levels: [
+                    {
+                        level: 1,
+                        infoType: "Workbook",
+                        resources: [
+                            {
+                                title: "Workbook fundamentos de HTML",
+                                type: "PDF",
+                                link: "https://drive.google.com/file/d/1LvWJ4uaMi-k2nsjSzqPAer4_zEOjXJ1n/view?usp=sharing",
+                            },
+                        ],
+                    },
+                    // Más niveles...
+                ],
+            },
+            {
+                name: "Html",
+                levels: [
+                    {
+                        level: 1,
+                        infoType: "Workbook",
+                        resources: [
+                            {
+                                title: "Workbook fundamentos de HTML",
+                                type: "PDF",
+                                link: "https://drive.google.com/file/d/1LvWJ4uaMi-k2nsjSzqPAer4_zEOjXJ1n/view?usp=sharing",
+                            },
+                        ],
+                    },
+                    // Más niveles...
+                ],
+            },
+            // Más tarjetas...
+        ],
+    },
+    {
+        program: "Programate School",
+        cards: [
+            {
+                name: "Logica de programacion",
+                levels: [
+                    {
+                        level: 1,
+                        infoType: "Workbook",
+                        resources: [
+                            {
+                                title: "Workbook Javascript",
+                                type: "PDF",
+                                link: "https://drive.google.com/file/d/1hemzW5EWBbPmtX9_cSWXvF0mgRKbAjYj/view?usp=sharing",
+                            },
+                        ],
+                    },
+                    // Más niveles...
+                ],
+            },
+            {
+                name: "CSS",
+                levels: [
+                    {
+                        level: 1,
+                        infoType: "Workbook",
+                        resources: [
+                            {
+                                title: "",
+                                type: "",
+                                link: "",
+                            },
+                        ],
+                    },
+                    // Más niveles...
+                ],
+            },
+            {
+                name: "Html",
+                levels: [
+                    {
+                        level: 1,
+                        infoType: "Workbook",
+                        resources: [
+                            {
+                                title: "Workbook fundamentos de HTML School",
+                                type: "PDF",
+                                link: "https://drive.google.com/file/d/1-Focvqf7bb6U_hi7_5PIPfniaPJTaYEF/view?usp=sharing",
+                            },
+                        ],
+                    },
+                    // Más niveles...
+                ],
+            },
+            // Más tarjetas...
+        ],
+    },
+    // Más programas...
+];
+
+export default InfoData;
