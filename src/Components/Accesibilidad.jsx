4b1286ef6a3b85df38cca8de57e57e8407ef715f
juancmb27 <juanmontealegreb.27@gmail.com>
2023-08-30 19:51:23 -0500

    Accesibilidad

--- src/Components/Accesibilidad.jsx
+++ src/Components/Accesibilidad.jsx
@@ -2,22 +2,20 @@ import React, { useState, useEffect } from 'react';
 import '../Styles/Accesibilidad.css';
 
 function AccessibilityMenu() {
-  const [fontSize, setFontSize] = useState(16); // Tamaño de fuente inicial en píxeles
-  const [grayscale, setGrayscale] = useState(false); // Escala de grises
-  const [highContrast, setHighContrast] = useState(false); // Alto contraste
+  const [fontSize, setFontSize] = useState(16);
+  const [grayscale, setGrayscale] = useState(false);
+  const [highContrast, setHighContrast] = useState(false);
+  const [menuVisible, setMenuVisible] = useState(false);
 
   useEffect(() => {
-    // Aplicar estilos de fuente dinámicos
     document.body.style.fontSize = `${fontSize}px`;
 
-    // Aplicar estilos de escala de grises
     if (grayscale) {
       document.body.style.filter = 'grayscale(100%)';
     } else {
       document.body.style.filter = 'none';
     }
 
-    // Aplicar estilos de alto contraste
     if (highContrast) {
       document.body.style.backgroundColor = 'black';
       document.body.style.color = 'white';
@@ -43,14 +41,28 @@ function AccessibilityMenu() {
     setHighContrast((prevValue) => !prevValue);
   };
 
+  const toggleMenu = () => {
+    setMenuVisible((prevValue) => !prevValue);
+  };
+
   return (
     <div className="accessibility-menu">
-      <button onClick={increaseFontSize}>Aumentar Texto</button>
-      <button onClick={decreaseFontSize}>Disminuir Texto</button>
-      <button onClick={toggleGrayscale}>Escala de Grises</button>
-      <button onClick={toggleHighContrast}>Alto Contraste</button>
+      <button onClick={toggleMenu} className="menu-button">
+        Accesibilidad
+      </button>
+      {menuVisible && (
+        <div className="menu-content">
+          <button onClick={increaseFontSize}>Aumentar Texto</button>
+          <button onClick={decreaseFontSize}>Disminuir Texto</button>
+          <button onClick={toggleGrayscale}>Escala de Grises</button>
+          <button onClick={toggleHighContrast}>Alto Contraste</button>
+        </div>
+      )}
     </div>
   );
 }
 
 export default AccessibilityMenu;
+
+
+
