42ad2472b30502aa008ccd3b7eea342ea5047708
Giovannyhs <giovannyhs@outlook.com>
2023-08-30 21:30:48 -0500

    cambios de imagenes

--- src/Pages/Inicio.jsx
+++ src/Pages/Inicio.jsx
@@ -1,64 +1,42 @@
-import React from 'react'
-import Header from "../Components/Header"
-import '../Styles/Inicio.css'
-import Footer from "../Components/Footer"
-import Slider from "../Components/Slider"
-import AccessibilityMenu from '../Components/Accesibilidad'
+import React from "react";
+import Header from "../Components/Header";
+import "../Styles/Inicio.css";
+import Footer from "../Components/Footer";
+import Slider from "../Components/Slider";
 
 function Inicio() {
   return (
-    <div className='Container-programate'>
+    <div className="Container-programate">
       <Header />
-      <AccessibilityMenu />
 
       <h1>Bienvenido a Programate</h1>
       <Slider />
-      <div className='Container-description'>
+      <div className="Container-description">
         <h1>QUIENES SOMOS</h1>
-        <p>Prográmate es un bootcamp de desarrollo web. <br />
-          Tiene como objetivo responder a las necesidades de formación de personas talentosas <br /> a las que les apasiona el mundo de la tecnología, y las demandas de las empresas <br /> que se encuentran en proceso de transformación y consolidación digital en el país.</p>
+        <p>
+          Prográmate es un bootcamp de desarrollo web. <br />
+          Tiene como objetivo responder a las necesidades de formación de
+          personas talentosas <br /> a las que les apasiona el mundo de la
+          tecnología, y las demandas de las empresas <br /> que se encuentran en
+          proceso de transformación y consolidación digital en el país.
+        </p>
         <button>INGRESAR</button>
       </div>
 
-      <div className='Container-video'>
-        <iframe width="100%" height="100%" src="https://www.youtube.com/embed/8oY2riIbxg4?si=1bzUky5r9v5r8s3p" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <div className="Container-video">
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/8oY2riIbxg4?si=1bzUky5r9v5r8s3p"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
       </div>
       <Footer />
-    </div>
-  )
-}
-
-export default Inicio
-
-
-/*import React, { useState } from 'react';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
-import Header from '../Components/Header';
-import "../Styles/Inicio.css"; // Se Importa el archivo de estilos CSS para las transiciones
-
-function Inicio() {
-  const [showHomePreview, setShowHomePreview] = useState(true);
-
-  const toggleHomePreview = () => {
-    setShowHomePreview(!showHomePreview);
-  };
-
-  return (
-    <div>
-      <button onClick={toggleHomePreview}>Toggle Preview</button>
-
-      <TransitionGroup>
-        {showHomePreview && (
-          <CSSTransition classNames="fade" timeout={300}>
-            <div>
-              <Header />
-              <h1>home preview</h1>
-            </div>
-          </CSSTransition>
-        )}
-      </TransitionGroup>
     </div>
   );
 }
 
-export default Inicio;*/
+export default Inicio;
