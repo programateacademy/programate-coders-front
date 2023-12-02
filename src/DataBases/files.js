05f4b8e3a7e2dfd57001e7bf689eca439a46f570
jayler0203 <jayler0203@gmail.com>
2023-12-02 12:17:16 -0500

    Reestructurar proyecto y subir nueva versión

---
+++ src/DataBases/files.js
@@ -0,0 +1,453 @@
+const InfoData = [
+  {
+    program: "Programate Academy",
+    cards: [
+        { 
+            item: 3,
+            name: "Html",
+            image: "Imagenes/html.svg",
+            playlist:
+              "https://www.youtube.com/playlist?list=PLHE0O79Fwx1IvMC9vhy4t5Z-OwgQwc4hz",
+            levels: [
+              {
+                level: 1,
+                infoType: [
+                  {
+                    Workbook: [
+                      {
+                        title: "",
+                        type: "PDF",
+                        link: "",
+                      },
+                    ],
+    
+                    proyectos: [
+                      {
+                        title: "Deezer 2",
+                        type: "URL",
+                        link: "https://www.deezer.com/mx/offers/premium",
+                      },
+                      {
+                        title: "Deezer 2",
+                        type: "URL",
+                        link: "https://www.deezer.com/mx/offers",
+                      },
+                    ],
+                    recursos: [
+                      {
+                        title: "Curso práctico",
+                        type: "URL",
+                        link: "http://html.net/",
+                      },
+                    ],
+                  },
+                ],
+              },
+              // Más niveles...
+            ],
+          },,
+      {
+        item: 2,
+        name: "CSS",
+        image: "Imagenes/css3.svg",
+        levels: [
+          {
+            level: 1,
+            infoType: [
+              {
+                Workbook: [
+                  {
+                    title: "",
+                    type: "PDF",
+                    link: "",
+                  },
+                ],
+                proyectos: [
+                  {
+                    title: "",
+                    type: "PDF",
+                    link: "",
+                  },
+                ],
+                recursos: [
+                  {
+                    title: "Flexbox Froggy",
+                    type: "URL",
+                    link: "https://flexboxfroggy.com/#es",
+                  },
+                  {
+                    title: "CSS battle",
+                    type: "URL",
+                    link: "https://cssbattle.dev/battle/1",
+                  },
+                  {
+                    title: "CSS Diner",
+                    type: "URL",
+                    link: "https://flukeout.github.io/",
+                  },
+                  {
+                    title: "Grid Garden",
+                    type: "URL",
+                    link: "https://cssgridgarden.com/#es",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            level: 2,
+            infoType: [
+              {
+                Workbook: [
+                  {
+                    title: "",
+                    type: "PDF",
+                    link: "",
+                  },
+                ],
+                proyectos: [
+                  {
+                    title: "Deezer 2",
+                    type: "URL",
+                    link: "https://www.deezer.com/mx/offers/premium",
+                  },
+                  {
+                    title: "Deezer 2",
+                    type: "URL",
+                    link: "https://www.deezer.com/mx/offers",
+                  },
+                ],
+                recursos: [
+                  {
+                    title: "Curso práctico",
+                    type: "URL",
+                    link: "http://html.net/",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            level: 3,
+            infoType: [
+              {
+                Workbook: [
+                  {
+                    title: "",
+                    type: "PDF",
+                    link: "",
+                  },
+                ],
+                proyectos: [
+                  {
+                    title: "",
+                    type: "PDF",
+                    link: "",
+                  },
+                ],
+                recursos: [
+                  {
+                    title: "",
+                    type: "PDF",
+                    link: "",
+                  },
+                ],
+              },
+            ],
+          },
+          // Más niveles...
+        ],
+      },
+      {
+        item: 1,
+        name: "JavaScript",
+        image: "Imagenes/js.svg",
+        playlist:
+          "https://www.youtube.com/playlist?list=PLHE0O79Fwx1LmbRvfZGNI_qlHlXy8C5Lw",
+        levels: [
+          {
+            level: 1,
+            infoType: [
+              {
+                Workbook: [
+                  {
+                    title: "Workbook Javascript",
+                    type: "PDF",
+                    link: "https://drive.google.com/file/d/1hemzW5EWBbPmtX9_cSWXvF0mgRKbAjYj/view?usp=sharing",
+                  },
+                  {
+                    title: "Workbook nivelacion JS",
+                    type: "PDF",
+                    link: "https://drive.google.com/file/d/1yly_Q1SQYTHGaEm0bZ5rXQyS0zYgRC-V/view?usp=sharing",
+                  },
+                  {
+                    title: "Wokbook JavaScript",
+                    type: "PDF",
+                    link: "https://drive.google.com/drive/folders/1U_47SMq8j8dru6w0CgomVTGg_XbGoNms",
+                  },
+                ],
+                proyectos: [],
+                recursos: [
+                  {
+                    title: "Programación orientada a objetos",
+                    type: "PDF",
+                    link: "https://drive.google.com/file/d/14eE35J1b9kcY_dTgCuLoTtQ27Nro1XxT/view?usp=sharing",
+                  },
+                  {
+                    title: "Ejercicios prácticos",
+                    type: "URL",
+                    link: "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            level: 2,
+            infoType: [
+              {
+                Workbook: [
+                  {
+                    title: "",
+                    type: "PDF",
+                    link: "",
+                  },
+                ],
+                proyectos: [
+                  {
+                    title: "",
+                    type: "PDF",
+                    link: "",
+                  },
+                ],
+                recursos: [
+                  {
+                    title: "",
+                    type: "PDF",
+                    link: "",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            level: 3,
+            infoType: [
+              {
+                Workbook: [
+                  {
+                    title: "",
+                    type: "PDF",
+                    link: "",
+                  },
+                ],
+                proyectos: [
+                  {
+                    title: "",
+                    type: "PDF",
+                    link: "",
+                  },
+                ],
+                recursos: [
+                  {
+                    title: "",
+                    type: "PDF",
+                    link: "",
+                  },
+                ],
+              },
+            ],
+          },
+          // Más niveles...
+        ],
+      }
+      
+      // Más tarjetas...
+    ],
+  },
+  {
+    program: "Programate School",
+    cards: [
+      {
+        item: 1,
+        name: "INICIO",
+        image: "Imagenes/welcome.svg",
+        levels: [
+          {
+            level: 1,
+            infoType: [
+              {
+                Workbook: [
+                  {
+                    title: "Workbook Desarrollo web",
+                    type: "PDF",
+                    link: "https://drive.google.com/file/d/17HAT9LYD1OGEWuAVjvwVq7UbIC12b-UN/view?usp=sharing",
+                  },
+                  {
+                    title: "Metodologia scrum",
+                    type: "PDF",
+                    link: "https://drive.google.com/file/d/1M1xAAAziyuTBXm7dfUSADTAHzvUDHP22/view?usp=sharing",
+                  },
+                ],
+                proyectos: [
+                  {
+                    title: "City for scrum",
+                    type: "PDF",
+                    link: "https://drive.google.com/file/d/1-VJnSPhcED6xQvHIffXI-WzdMbaJv0RP/view?usp=sharing",
+                  },
+                ],
+                recursos: [
+                  {
+                    title: "Metodologia scrum",
+                    type: "VIDEO",
+                    link: "https://drive.google.com/file/d/18zOHHM4S0-OPKEA9pIM2gFatMCVGkSc5/view?usp=sharing",
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        item: 2,
+        name: "Logica de programacion",
+        image: "Imagenes/logica.svg",
+        levels: [
+          {
+            level: 1,
+            infoType: [
+              {
+                Workbook: [
+                  {
+                    title: "Workbook Lgica de programacion",
+                    type: "PDF",
+                    link: "https://drive.google.com/file/d/1aOWXvUaU43QiV-4AZGUPoxg90F7UD8qw/view?usp=sharing",
+                  },
+                ],
+                proyectos: [],
+                recursos: [
+                  {
+                    title: "Algoritmos",
+                    type: "PDF",
+                    link: "https://drive.google.com/file/d/1KPomu_FaMphW5LTQcID2yF94FY2EuSfm/view?usp=sharing",
+                  },
+                  {
+                    title: "Algoritmos",
+                    type: "PDF",
+                    link: "https://drive.google.com/file/d/1hvPqZfk4HTqZN16pMH4JfUrfiKF4Pogp/view?usp=sharing",
+                  },
+                  {
+                    title: "Algoritmos",
+                    type: "VIDEO",
+                    link: "https://drive.google.com/file/d/1rMz2jDio51y4-JH2yvMf1LWZpJBruvr3/view?usp=sharing",
+                  },
+                ],
+              },
+            ],
+          },
+          // Más niveles...
+        ],
+      },
+      {
+        item: 3,
+        name: "HTML",
+        image: "Imagenes/html.svg",
+        levels: [
+          {
+            level: 1,
+            infoType: [
+              {
+                Workbook: [
+                  {
+                    title: "Workbook Fundamentos en HTML",
+                    type: "PDF",
+                    link: "https://drive.google.com/file/d/1-Focvqf7bb6U_hi7_5PIPfniaPJTaYEF/view?usp=sharing",
+                  },
+                ],
+                proyectos: [],
+                recursos: [
+                  {
+                    title: "HTML intro",
+                    type: "VIDEO",
+                    link: "https://drive.google.com/file/d/1yW29nafJMt4WW_3JziE-VUJBXnNFYGD2/view?usp=sharing",
+                  },
+                  {
+                    title: "Etiqueta de enlace",
+                    type: "VIDEO",
+                    link: "https://drive.google.com/file/d/1pmriCWE6t3r7yLvMEzyr1yETkRf_O2nH/view?usp=sharing",
+                  },
+                  {
+                    title: "Etiqueta de imagen",
+                    type: "VIDEO",
+                    link: "https://drive.google.com/file/d/1Ch39N1ASVie-FMzvZ8oFrnlUpy6FUpFs/view?usp=sharing",
+                  },
+                  {
+                    title: "Etiqueta de listas",
+                    type: "VIDEO",
+                    link: "https://drive.google.com/file/d/1FGK7NobzsC5a9vjZoW5nAuhx4dPPVHVo/view?usp=sharing",
+                  },
+                  {
+                    title: "Etiqueta de parrafo",
+                    type: "VIDEO",
+                    link: "https://drive.google.com/file/d/1ZKvxtIwpJGwyeiTtBInfxP_kN0DAvdMk/view?usp=sharing",
+                  },
+                  {
+                    title: "Etiqueta de audio y video",
+                    type: "VIDEO",
+                    link: "https://drive.google.com/file/d/1dF44ZVMNVhfW_2uXk_HNVJkgoQiFcleF/view?usp=sharing",
+                  },
+                  {
+                    title: "Etiqueta de encabezado",
+                    type: "VIDEO",
+                    link: "https://drive.google.com/file/d/1FWPModcpIgZPNZme4zoXe_cA9IUIYqgD/view?usp=sharing",
+                  },
+                  {
+                    title: "Etiqueta de formato de texto",
+                    type: "VIDEO",
+                    link: "https://drive.google.com/file/d/1yKwsslqQQ-uKaBVy-mLmWc7EzZ2xfcXe/view?usp=sharing",
+                  },
+                ],
+              },
+            ],
+          },
+          // Más niveles...
+        ],
+      },
+
+      {
+        item: 4,
+        name: "GitHub",
+        image: "Imagenes/github.svg",
+        levels: [
+          {
+            level: 1,
+            infoType: [
+              {
+                Workbook: [
+                  {
+                    title: "Workbook Primeros pasos con Git-Hub",
+                    type: "PDF",
+                    link: "https://drive.google.com/file/d/1DxLN2mxWn3Asf67xjamXZ3YBVJkrtoRl/view?usp=sharing",
+                  },
+                ],
+              },
+            ],
+            resources: [
+              {
+                title: "Comandos de Git-Hub",
+                type: "PDF",
+                link: "https://drive.google.com/file/d/1gXOviSb1pXpb4_tK6fR6iiLWZF_iVX8F/view?usp=sharing",
+              },
+            ],
+          },
+          // Más niveles...
+        ],
+      },
+
+      // Más tarjetas...
+    ],
+  },
+  // Más programas...
+];
+
+export default InfoData;
