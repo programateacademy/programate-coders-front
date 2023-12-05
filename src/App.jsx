988fcc49d4aa111710f4ad1933f089d69c322e71
iamsebas376 <londonosebas201.sloa@gmail.com>
2023-12-04 22:01:52 -0500

    feat: 1. se agrega la sección de cards de la página programas 2. Se agregan los JSON actualizados

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-
+import { useState } from "react";
+import ProgramCards from "./components/organisms/programCards/Academy/ProgramCardsAcademyAcademy.jsx";
 
 function App() {
-
   return (
     <>
+      <ProgramCards />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/DataBases/programateAcademyData.js
@@ -0,0 +1,181 @@
+const programateAcademyData = [
+    "videos" = [
+        {
+            item: 1,
+            title: "Curso JavaScript",
+            language: "JavaScript",
+            program: "Programate Academy",
+            link: "https://www.youtube.com/playlist?list=PLHE0O79Fwx1LmbRvfZGNI_qlHlXy8C5Lw"
+        },
+        {
+            item: 2,
+            title: "Curso React",
+            language: "React",
+            program: "Programate Academy",
+            link: "https://www.youtube.com/playlist?list=PLHE0O79Fwx1K6tyzV1zfFqVrHGOdWGMm3"
+        },
+        {
+            item: 3,
+            title: "Curso HTML",
+            language: "Html",
+            program: "Programate Academy",
+            link: "https://www.youtube.com/playlist?list=PLHE0O79Fwx1IvMC9vhy4t5Z-OwgQwc4hz"
+        },
+        {
+            item: 4,
+            title: "Curso CSS",
+            language: "CSS",
+            program: "Programate Academy",
+            link: "https://www.youtube.com/playlist?list=PLHE0O79Fwx1Ivc_DdpVLwlnfilOO-_2Jb"
+        }
+    ],
+
+    "workbooks" = [
+        {
+            item: 1,
+            title: "Fundamentos Html",
+            language: "HTML",
+            program: "Programate Academy",
+            link: "https://drive.google.com/file/d/1BkbcNeil0w3SXvvoHm527hpVa2Tp99na/view?usp=drive_link"
+        },
+        {
+            item: 2,
+            title: "Lógica de programacion",
+            language: "Diagramas de flujo",
+            program: "Programate Academy",
+            link: "https://drive.google.com/file/d/1RsRUDLq9t3aU-MfEKVOfdIRc71sjhrGz/view?usp=drive_link"
+        },
+        {
+            item: 3,
+            title: "Condicionales if else y ciclos for Python",
+            language: "Python",
+            program: "Programate Academy",
+            link: "https://drive.google.com/file/d/1xB8rM3yZlV7_DWWA4Gmix3gOTob1BVka/view?usp=drive_link"
+        },
+        {
+            item: 4,
+            title: "Tipos de datos en Python",
+            language: "Python",
+            program: "Programate Academy",
+            link: "https://drive.google.com/file/d/1R3yBmpw9A_XSb066U9L86fPEo9IVSOSC/view?usp=drive_link"
+        },
+        {
+            item: 5,
+            title: "Resolucion de conflictos",
+            language: "Método OIS",
+            program: "Programate Academy",
+            link: "https://drive.google.com/file/d/12jLHGCAWCzJkpVx-1xiATRgfVnqcQfx-/view?usp=drive_link"
+        },
+        {
+            item: 6,
+            title: "Agile y Scrum",
+            language: "Metodología Scrum",
+            program: "Programate Academy",
+            link: "https://drive.google.com/file/d/18AA3jMqn8qXI97JO_LHzya4OoxhDzTRe/view?usp=drive_link"
+        },
+        {
+            item: 7,
+            title: "Pedagogía activa y basada en proyectos",
+            language: "Pedagogía activa",
+            program: "Programate Academy",
+            link: "https://drive.google.com/file/d/1IcG4NT90AbzJbQVWtRVFOVo5ujLdl-Sf/view?usp=drive_link"
+        },
+        {
+            item: 8,
+            title: "Álgebra booleana",
+            language: "Álgebra",
+            program: "Programate Academy",
+            link: "https://drive.google.com/file/d/1lU4xJ-HZjbTx-6G677DYNOH_xgP3vIjZ/view?usp=drive_link"
+        },
+        {
+            item: 9,
+            title: "Visual studio code",
+            language: "Vs code",
+            program: "Programate Academy",
+            link: "https://drive.google.com/file/d/1-f2sbB5wlynj8QHU_el7__34PShNvEIA/view?usp=drive_link"
+        },
+        {
+            item: 10,
+            title: "POP-OS",
+            language: "POP-OS",
+            program: "Programate Academy",
+            link: "https://drive.google.com/file/d/1LK3P9mEc73SMDfMuycZ39mVOYSOmsGAG/view?usp=drive_link"
+        },
+        {
+            item: 11,
+            title: "Git Bash",
+            language: "Git",
+            program: "Programate Academy",
+            link: "https://drive.google.com/file/d/1ZTGd-uD3sTQ41cqLPXNrN69TY5O4paQf/view?usp=drive_link"
+        },
+        {
+            item: 12,
+            title: "Conventional Commits",
+            language: "Git",
+            program: "Programate Academy",
+            link: "https://drive.google.com/file/d/1L2L4dnD47bdQiRgyDR_I2o6nBm1HbZGU/view?usp=drive_link"
+        },
+        {
+            item: 13,
+            title: "Diseño de interfaces",
+            language: "Diseño",
+            program: "Programate Academy",
+            link: "https://drive.google.com/file/d/1NB6ZKLQXsgld1LnPNj8CVGDoSdbvi7G9/view?usp=drive_link"
+        }, {
+            item: 14,
+            title: "Búsquedas efectivas en internet",
+            language: "Búsquedas",
+            program: "Programate Academy",
+            link: "https://drive.google.com/file/d/13JBynrQ-Loh-gk2qk_d4yHTFs8LDpCCT/view?usp=drive_link"
+        }, {
+            item: 15,
+            title: "Clean Code",
+            language: "Clean Code",
+            program: "Programate Academy",
+            link: "https://drive.google.com/file/d/1lnIVczki4x7kxlJSIoxpthz9M-63Mdi-/view?usp=drive_link"
+        }, {
+            item: 16,
+            title: "Fundamentos de CSS",
+            language: "CSS",
+            program: "Programate Academy",
+            link: "https://drive.google.com/file/d/1ok-8dWaJ0QchwNNsoRYT-DuTQhRQ5l_e/view?usp=drive_link"
+        },
+        {
+            item: 17,
+            title: "Programación orientada a objetos",
+            language: "POO",
+            program: "Programate Academy",
+            link: "https://drive.google.com/file/d/1FyiqYIzGmm6q_QC-rX42TsamcaGZ0ZA6/view?usp=drive_link"
+        },
+        {
+            item: 18,
+            title: "Uso de Terminal",
+            language: "Terminal",
+            program: "Programate Academy",
+            link: "https://drive.google.com/file/d/1pFdTmYpC0jeMdcbbzrNfEnhSV8aA289x/view?usp=drive_link"
+        },
+        {
+            item: 19,
+            title: "GitHub",
+            language: "GitHub",
+            program: "Programate Academy",
+            link: "https://drive.google.com/file/d/11lsYOh1m6bWGX54yEvW7LQeXLZEglCNf/view?usp=drive_link"
+        },
+        {
+            item: 20,
+            title: "JavaScript",
+            language: "JavaScript",
+            program: "Programate Academy",
+            link: "https://drive.google.com/file/d/12Nz8yrOCypayf21fTd5ww2-nPuE3n43E/view?usp=drive_link"
+        },
+        {
+            item: 21,
+            title: "Node y ExpressJS",
+            language: "Node JS y Express JS",
+            program: "Programate Academy",
+            link: "https://drive.google.com/file/d/1vfltq0GvcuoSSfwruzHnNjbu_elCXehy/view?usp=drive_link"
+        }
+
+    ]
+]
+export default programateAcademyData;
