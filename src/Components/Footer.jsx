73f000ac6d4751a33dc005ca2333b3925f6014a5
Cesaringeniero001 <cesar.penuela@hotmail.com>
2023-12-05 17:58:35 -0500

    First modification on footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -10,40 +10,61 @@ import {
 
 function Footer() {
     return (
-        <div className="footer">
-        <img
-          className="logo"
-          src="./Imagenes/logo-a-color--removebg-preview.png"
-          alt=""
-        />
+      <div className="footer">
+        <div className="footer1">
+            <img
+              className="logo"
+              src="./Imagenes/logo-a-color--removebg-preview.png"
+              alt=""
+            />
+          
 
-      <div className="social-media">
-        <a
-          className="redes"
-          href="https://web.facebook.com/somoseducamas/?_rdc=1&_rdr"
-        >
-          <BiLogoFacebook />
-        </a>
-        <a className="redes" href="https://www.instagram.com/somoseducamas/">
-          <BiLogoInstagram />
-        </a>
-        <a
-          className="redes"
-          href="https://www.linkedin.com/company/educam%C3%A1s/"
-        >
-          <BiLogoLinkedin />
-        </a>
-        <a className="redes" href="">
-          <BiLogoTiktok />
-        </a>
-        <a
-          className="redes"
-          href="https://open.spotify.com/show/4mYZpSGP4SJMEDvFilTNRp?si=5bNY3MFmTpS1Fq5GtiUpag.+&nd=1"
-        >
-          <BiLogoSpotify />
-        </a>
+          <div className="number">3114508935</div>
+
+          <div className="direction">Cra. 13#83-19 Bogotá, Colombia.</div>
+        </div>
+        <hr className="footer-divider" />
+        <div className="footer2">
+          <div className="social-media">
+            <div className="facebook">
+              <a
+                className="redes"
+                href="https://web.facebook.com/somoseducamas/?_rdc=1&_rdr">
+                <BiLogoFacebook />
+              </a>
+            </div>
+            <div className="instagram">
+              <a className="redes" href="https://www.instagram.com/somoseducamas/">
+                <BiLogoInstagram />
+              </a>
+            </div>
+            <div className="linkedin">
+              <a
+                className="redes"
+                href="https://www.linkedin.com/company/educam%C3%A1s/"
+              >
+                <BiLogoLinkedin />
+              </a>
+            </div>
+            <div className="tiktok">
+              <a className="redes" href="">
+                <BiLogoTiktok />
+              </a>
+            </div>
+            <div className="spotify">
+              <a
+                className="redes"
+                href="https://open.spotify.com/show/4mYZpSGP4SJMEDvFilTNRp?si=5bNY3MFmTpS1Fq5GtiUpag.+&nd=1"
+              >
+                <BiLogoSpotify />
+              </a>
+            </div>
+          </div>
+          <div className="footer-bottom">
+              Copyrigth&copy;2023 Desarrollado por Educamas
+          </div>
+        </div>  
       </div>
-    </div>
   );
 }
 
