900815bbd34deb0a2840abb1ba937a07c1d6f571
iamsebas376 <londonosebas201.sloa@gmail.com>
2023-12-12 21:11:00 -0500

    fix: change pages routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/organisms/header/Header.jsx";
 import Footer from "./components/organisms/Footer/Footer.jsx";
-import Inicio from "./components/pages/Inicio/Inicio.jsx";
+import Home from "./components/pages/Home/Home.jsx";
 import SobreNosotros from "./components/pages/SobreNosotros/SobreNosotros.jsx";
 import Programas from "./components/pages/Programas/Programas.jsx";
 import Contactanos from "./components/pages/Contactanos/Contactanos.jsx";
@@ -12,7 +12,7 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path="/" element={<Inicio />} />
+          <Route path="/" element={<Home />} />
           <Route path="/sobre-nosotros" element={<SobreNosotros />} />
           <Route path="/programas" element={<Programas />} />
           <Route path="/contacto" element={<Contactanos />} />
