00221637a921b8e287311dedf13aca9aba3370c2
ampros94 <elqueledije@gmail.com>
2023-12-13 21:41:57 -0500

    add changes home

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,11 +1,17 @@
 import React from "react"
 import Slider from "./components/slider.jsx"
+import Container1 from "./components/container1/container1.jsx"
+import Container2 from "./components/container2/container2.jsx"
+import Container3 from "./components/container3/container3.jsx"
 
 function Home() {
 
-  return (
+  return <>
     <Slider></Slider>
-    )
+    <Container1></Container1>
+    <Container2></Container2>
+    <Container3></Container3>
+    </>
 }
 
 export default Home
