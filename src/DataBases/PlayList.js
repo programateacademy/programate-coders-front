a16dcaa4d1232f6d1ab72ad1a52f6eb93ea8e0e4
Cesaringeniero001 <cesar.penuela@hotmail.com>
2023-12-14 17:06:06 -0500

    updating main branch

---
+++ src/DataBases/PlayList.js
@@ -0,0 +1,31 @@
+const playListData = [ 
+    {
+        item: 1,
+        title: "Curso JavaScript",
+        language: "JavaScript",
+        program: "Programate Academy",
+        link: "https://www.youtube.com/playlist?list=PLHE0O79Fwx1LmbRvfZGNI_qlHlXy8C5Lw"
+    },
+    {
+        item: 2,
+        title: "Curso React",
+        language: "React",
+        program: "Programate Academy",
+        link: "https://www.youtube.com/playlist?list=PLHE0O79Fwx1K6tyzV1zfFqVrHGOdWGMm3"
+    },
+    {
+        item: 3,
+        title: "Curso HTML",
+        language: "Html",
+        program: "Programate Academy",
+        link: "https://www.youtube.com/playlist?list=PLHE0O79Fwx1IvMC9vhy4t5Z-OwgQwc4hz"
+    },
+    {
+        item: 4,
+        title: "Curso CSS",
+        language: "CSS",
+        program: "Programate Academy",
+        link: "https://www.youtube.com/playlist?list=PLHE0O79Fwx1Ivc_DdpVLwlnfilOO-_2Jb"
+    }
+]
+export  default playListData
---
+++ src/components/organisms/PanelSources/PanelSources.jsx
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from "react";
+import ListSources from "../../molecules/ListSources/ListSources";
+import TabList from "../../molecules/tabList/TabList";
+import ReactPlayer from "react-player";
+import { videosStore } from "../../../store/videosStore";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+//solicitud a la Api de Youtube para traer las listas de reproducción
+const fetchData = async (language, playList, setSelectedVideo) => {
+  const apiKey = "AIzaSyCOgAm7ywQ9rYOF20uRC3HlKT3BjDKaXLQ";
+  const extractID = (playlistLink) => {
+    const urlParts = playlistLink.split("?");
+    const params = new URLSearchParams(urlParts[1]);
+    return params.get("list");
+  };
+  const filterLinks = playList.find((item) => item.language === language);
+  const playlistId = extractID(filterLinks.link);
+
+  const response = await axios.get(
+    `https://www.googleapis.com/youtube/v3/playlistItems`,
+    {
+      params: {
+        part: "snippet",
+        playlistId: playlistId,
+        key: apiKey,
+        maxResults: 10000, // validar si este parametro es necesario
+      },
+    }
+  );
+  if (response.data.items.length > 0) {
+    const defaultVideoId = response.data.items[0].snippet.resourceId.videoId;
+    setSelectedVideo(defaultVideoId);
+  }
+
+  return response.data.items;
+};
+
+function PanelSources() {
+  const { setSelectedVideo, SelectedVideo, language, playList } = videosStore();
+
+  const {
+    data: videos,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["currentPlayList"],
+    queryFn: () => fetchData(language, playList, setSelectedVideo),
+  });
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (isError) {
+    return <p>{`${error}`}</p>;
+  }
+
+  if (!videos || videos.length === 0) {
+    return <p>No videos found in the playlist</p>;
+  }
+
+  return (
+    <div className="panel-sources">
+      <div className="player-container">
+      <ReactPlayer 
+        controls={true}
+        width={"100%"}
+        height={"100%"}
+        url={`https://www.youtube.com/watch?v=${SelectedVideo}`}
+      ></ReactPlayer>
+    </div>
+      <div className="tabSources">
+      <TabList />
+      <ListSources/>
+      </div>
+
+
+    </div>
+  );
+}
+
+export default PanelSources;
