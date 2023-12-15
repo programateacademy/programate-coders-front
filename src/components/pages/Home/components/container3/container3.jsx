89be40a96d9b18cbeea254419c6a0646f096fd17
ampros94 <elqueledije@gmail.com>
2023-12-15 18:35:46 -0500

    changes home

--- src/components/pages/Home/components/container3/container3.jsx
+++ src/components/pages/Home/components/container3/container3.jsx
@@ -4,9 +4,15 @@ import './styleContainer3.scss';
 function Container3() {
     return (
         <div className='container3'>
-            <div className='info-cont3'>
                 <h1 className='title-info-cont3'>Conocer algunas historias</h1>
+            <div className='info-cont3'>
                 <div className='history'>
+                    <h2>Nombre</h2>
+                    <p>Gracias al Programa, ahora estoy construyendo sitios web. 
+                    ¡Excelente experiencia de aprendizaje!</p>
+                    <img src="src\assets\Imagenes\commits.svg" alt="" />
+                </div>
+                <div className='history' id='history2'>
                     <h2>Nombre</h2>
                     <p>Gracias al Programa, ahora estoy construyendo sitios web. 
                     ¡Excelente experiencia de aprendizaje!</p>
