15c2eac35864ec47520d84bcfe5557cb37b2bd80
iamsebas376 <londonosebas201.sloa@gmail.com>
2023-12-15 22:16:20 -0500

    fix: fonts

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.scss";
-import Typography from "./Styles/Typography.js";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Typography />
       <App />
     </QueryClientProvider>
   </React.StrictMode>
