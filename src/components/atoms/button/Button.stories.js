a6649494ed6c6d60c6ebaba7859e45ada0442139
jayler0203 <jayler0203@gmail.com>
2023-12-19 16:13:13 -0500

    feat:create stories button and add scripts for docs

--- src/components/atoms/button/Button.stories.js
+++ src/components/atoms/button/Button.stories.js
@@ -1,5 +1,5 @@
 import Button from "./Button";
-import "./Button.styles.scss"
+import "./Button.styles.scss";
 export default {
   title: "Components/atoms/Button",
   component: Button,
@@ -9,23 +9,65 @@ export default {
   tags: ["autodocs"],
   argTypes: {
     children: "Boton",
-    className:""
+    className: "",
   },
 };
-  /**
-   * Boton para seleccionar la lista de recursos que se quiere mostrar
-   */
+/**
+ * Boton para seleccionar la lista de recursos que se quiere mostrar
+ */
+export const ButtonPrimary={
+  args:{
+    className: "button-primary",
+    children: "Boton",
+  },
+}
+/**
+ * Boton para seleccionar la lista de recursos que se quiere mostrar
+ */
 export const ButtonTab = {
   args: {
     className: "button-tab",
-    children: "tab button"
-
+    children: "tab button",
   },
 };
+/**
+ * Boton para mostrar contenido adicional 
+ */
 export const ButtonShow = {
-    args: {
-      className: "button-show",
-      children: "ver mas"
+  args: {
+    className: "button-show",
+    children: "ver mas",
+  },
+};
+/**
+ * Boton
+ */
+export const ButtonSlider = {
+  args: {
+    className: "button-slider",
+    children: "¡Explora Programas!",
+  },
+};
+/**
+ * Boton 
+ */
+export const ButtonCard = {
+  args: {
+    className: "button-card",
+    children: "Ver programa",
+  },
+};
+export const ButtonForm={
+  args: {
+    className: "button-form",
+    children: "Enviar",
+  },
+
+}
+export const ButtonProgram={
+  args: {
+    className: "program-button",
+    children: "Iniciar programa",
+  },
   
-    },
-  };
+}
