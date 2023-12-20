38d9287f2af522f100f7e2536cb298687988ced8
iamsebas376 <londonosebas201.sloa@gmail.com>
2023-12-20 16:18:17 -0500

    fix: styles

--- src/components/pages/About/About.jsx
+++ src/components/pages/About/About.jsx
@@ -1,61 +1,66 @@
-import React from 'react';
-import NumeroAnimado from './componentes/numeroAnimado';
-
+import React from "react";
+import NumeroAnimado from "./componentes/numeroAnimado";
 
 function About() {
   return (
-
     <div className="container-about">
-      <div className="container1">
-        <div className="title">¿Quiénes Somos?
+      <div className="content-about">
+        <div className="container-1">
+          <div className="title">¿Quiénes Somos?</div>
+          <br />
+          <p className="about-text">
+            Somos una escuela de programación inclusiva y solidaria. Formamos el
+            talento que hará posible la transformación digital de nuestro país,{" "}
+            <span className="subrayado">
+              nuestro principal objetivo es que logres desarrollar tu potencial
+              para el sector tech y lleves tu talento al siguiente nivel.
+            </span>
+          </p>
         </div>
-        <br/>
-        <p className="about-text">Somos una escuela de programación inclusiva y solidaria. Formamos el talento
-          que hará posible la transformación digital de nuestro país, <span className="subrayado">nuestro principal 
-          objetivo es que logres desarrollar tu potencial para el sector tech y lleves 
-          tu talento al siguiente nivel.</span>
-        </p>
-      </div>
-      
-      <div className="container2">
+        <div className="container-2">
           <div className="info-educamas">
             <div className="info-graduados">
-              <div className="contador-graduados"><NumeroAnimado numero={130} />+</div>
-              
+              <div className="contador-graduados">
+                <NumeroAnimado numero={130} />+
+              </div>
+
               <h4 className="title-graduados">Graduados</h4>
-              
-              <div className="text-graduados">¿Quieres ser uno de ellos?  Conoce nuestros programas
-              y cambia tu vida a través de la programación.
+
+              <div className="text-graduados">
+                ¿Quieres ser uno de ellos? Conoce nuestros programas y cambia tu
+                vida a través de la programación.
               </div>
             </div>
-            
-            <div className='info-empleabilidad'>
-                <div class="porcentaje-empleados"><NumeroAnimado numero={88} />%</div>
-                
-                <h4 className='title-empleabilidad'>Empleabilidad</h4>
-              
-                <div className="text-empleabilidad">De nuestros estudiantes consiguen un empleo al finalizar su formación</div>
-                
+
+            <div className="info-empleabilidad">
+              <div class="porcentaje-empleados">
+                <NumeroAnimado numero={88} />%
+              </div>
+
+              <h4 className="title-empleabilidad">Empleabilidad</h4>
+
+              <div className="text-empleabilidad">
+                De nuestros estudiantes consiguen un empleo al finalizar su
+                formación
+              </div>
             </div>
-            <div className='empresas-aliadas'>
-                <div class="contador-empresas"><NumeroAnimado numero={30} />+</div>
-                
-                <h4 className='title-empresas-aliadas'>Empresas aliadas</h4>
-                
-                <div className="text-empresas-aliadas">Generamos alianzas con empresas para que puedas tener tu primera oportunidad en el mundo tech</div>
-                
+            <div className="empresas-aliadas">
+              <div class="contador-empresas">
+                <NumeroAnimado numero={30} />+
+              </div>
+
+              <h4 className="title-empresas-aliadas">Empresas aliadas</h4>
+
+              <div className="text-empresas-aliadas">
+                Generamos alianzas con empresas para que puedas tener tu primera
+                oportunidad en el mundo tech
+              </div>
             </div>
           </div>
+        </div>
       </div>
-      
-
     </div>
-  
-
-
-  )
-
-  
+  );
 }
 
 export default About;
